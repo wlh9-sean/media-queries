7f502b38257b6ea29a120489b35ad9596ac3acd0
Sean Schouten <seanschouten@Seans-MacBook-Pro.local>
2019-09-27 12:38:25 -0600

    Completed project

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,24 +11,40 @@ export default class Navbar extends Component {
     }
 
     slide = () => {
-        this.setState = {
+        this.setState({
             menu: !this.state.menu
-        }
+        })
     }
     render() {
-        
         return (
             <div>
                 {/* Navbar */}
                 <nav className='navbar'>
                     <h1 className='title'>Start Bootstrap</h1>
-                    <div className='hamburger' onClick={this.slide}>&#9776;</div>
-                        <link>Services</link>
-                        <link>Portfolio</link>
-                        <link>About</link>
-                        <link>Team</link>
-                        <link>Contact</link>
+                    <div className='hamburger' onClick={this.slide}>
+                        &#9776;
+                    </div>
+                    <div className='menu'>
+                        <p>Services</p>
+                        <p>Portfolio</p>
+                        <p>About</p>
+                        <p>Team</p>
+                        <p>Contact</p>
+                    </div>
                 </nav>
+
+                {/* Slide Out */}
+                <div className={
+                    this.state.menu ?
+                    'slideMenu slide' :
+                    'slideMenu'
+                }>
+                   <p>Services</p> 
+                   <p>Portfolio</p> 
+                   <p>About</p> 
+                   <p>Team</p> 
+                   <p>Contact</p> 
+                </div>
             </div>
         )
     }
